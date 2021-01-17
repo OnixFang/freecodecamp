7fb3f0cf2f1731c7b5d7dffd935cb4b5ff847c64
Ulises Taveras <twily.dev@outlook.com>
2021-01-16 21:37:22 -0400

    Setup basic server pages

--- server/server.js
+++ server/server.js
@@ -5,8 +5,14 @@ const app = express();
 
 // Environment variables
 const { HOST, PORT } = process.env;
+// Static folder
+const publicPath = path.join(__dirname, '../public');
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(publicPath));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(publicPath, 'notfound.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on: http://${HOST}:${PORT}`);
